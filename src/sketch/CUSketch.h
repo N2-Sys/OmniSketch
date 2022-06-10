559683105685af60ed084a19867562e783318d9c
丁睿 <dromniscience@gmail.com>
2022-06-11 01:11:58 +0800

    Add CU Sketch

---
+++ src/sketch/CUSketch.h
@@ -0,0 +1,133 @@
+/**
+ * @file CUSketch.h
+ * @author dromniscience (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-06-10
+ *
+ *
+ */
+#pragma once
+
+#include <common/hash.h>
+#include <common/sketch.h>
+
+namespace OmniSketch::Sketch {
+/**
+ * @brief CU Sketch
+ *
+ * @tparam key_len  length of flowkey
+ * @tparam T        type of the counter
+ * @tparam hash_t   hashing class
+ */
+template <int32_t key_len, typename T, typename hash_t = Hash::AwareHash>
+class CUSketch : public SketchBase<key_len, T> {
+private:
+  int32_t depth;
+  int32_t width;
+  hash_t *hash_fns;
+  T **counter;
+
+  CUSketch(const CUSketch &) = delete;
+  CUSketch(CUSketch &&) = delete;
+
+public:
+  /**
+   * @brief Construct by specifying depth and width
+   *
+   */
+  CUSketch(int32_t depth_, int32_t width_);
+  /**
+   * @brief Release the pointer
+   *
+   */
+  ~CUSketch();
+  /**
+   * @brief Update a flowkey with certain value
+   *
+   */
+  void update(const FlowKey<key_len> &flowkey, T val) override;
+  /**
+   * @brief Query a flowkey
+   *
+   */
+  T query(const FlowKey<key_len> &flowkey) const override;
+  /**
+   * @brief Get the size of the sketch
+   *
+   */
+  size_t size() const override;
+  /**
+   * @brief Reset the sketch
+   *
+   */
+  void clear();
+};
+
+} // namespace OmniSketch::Sketch
+
+//-----------------------------------------------------------------------------
+//
+///                        Implementation of templated methods
+//
+//-----------------------------------------------------------------------------
+
+namespace OmniSketch::Sketch {
+template <int32_t key_len, typename T, typename hash_t>
+CUSketch<key_len, T, hash_t>::CUSketch(int32_t depth_, int32_t width_)
+    : depth(depth_), width(Util::NextPrime(width_)) {
+  hash_fns = new hash_t[depth];
+  // Allocate continuous memory
+  counter = new T *[depth];
+  counter[0] = new T[depth * width](); // Init with zero
+  for (int32_t i = 1; i < depth; ++i) {
+    counter[i] = counter[i - 1] + width;
+  }
+}
+
+template <int32_t key_len, typename T, typename hash_t>
+CUSketch<key_len, T, hash_t>::~CUSketch() {
+  delete[] hash_fns;
+  delete[] counter[0];
+  delete[] counter;
+}
+
+template <int32_t key_len, typename T, typename hash_t>
+void CUSketch<key_len, T, hash_t>::update(const FlowKey<key_len> &flowkey,
+                                          T val) {
+  int32_t indices[depth];
+  T min_val = std::numeric_limits<T>::max();
+  for (int32_t i = 0; i < depth; ++i) {
+    int32_t idx = hash_fns[i](flowkey) % width;
+    indices[i] = idx;
+    min_val = std::min(min_val, counter[i][idx]);
+  }
+  min_val += val;
+  for (int32_t i = 0; i < depth; ++i) {
+    counter[i][indices[i]] = std::max(min_val, counter[i][indices[i]]);
+  }
+}
+
+template <int32_t key_len, typename T, typename hash_t>
+T CUSketch<key_len, T, hash_t>::query(const FlowKey<key_len> &flowkey) const {
+  T min_val = std::numeric_limits<T>::max();
+  for (int32_t i = 0; i < depth; ++i) {
+    int32_t index = hash_fns[i](flowkey) % width;
+    min_val = std::min(min_val, counter[i][index]);
+  }
+  return min_val;
+}
+
+template <int32_t key_len, typename T, typename hash_t>
+size_t CUSketch<key_len, T, hash_t>::size() const {
+  return sizeof(*this)                // instance
+         + depth * sizeof(hash_t)     // hashing class
+         + depth * width * sizeof(T); // counter
+}
+
+template <int32_t key_len, typename T, typename hash_t>
+void CUSketch<key_len, T, hash_t>::clear() {
+  std::fill(counter[0], counter[0] + depth * width, 0);
+}
+
+} // namespace OmniSketch::Sketch
